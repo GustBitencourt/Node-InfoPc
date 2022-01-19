eee34538094ed76de71f4ed51fc3918ab57bb625
GustBitencourt <gubiten@gmail.com>
2022-01-19 18:11:55 -0300

    importacao do stats, criacao de rota no server

--- pcRamUsage.js
+++ pcRamUsage.js
@@ -1,14 +1,24 @@
 const os = require('os');
 
-const { arch, platform, totalmem, freemem } = os;
-const tRam = totalmem() / 1024 / 1024;
-const fRam = freemem() / 1024 / 1024;
+setInterval(() => {
+    const { arch, platform, totalmem, freemem } = os;
+    const tRam = totalmem() / 1024 / 1024;
+    const fRam = freemem() / 1024 / 1024;
+    const usage = (fRam / tRam) * 100;
+    
+    const stats = {
+        OS: platform(),
+        Arch: arch(),
+        TotalMem: `${parseInt(tRam)} MB`,
+        Freemem: `${parseInt(fRam)} MB`,
+        Usage: `${usage.toFixed(2)} %`
+    }
 
-const stats = {
-    OS: platform(),
-    Arch: arch(),
-    TotalMem: parseInt(tRam),
-    Freemem: parseInt(fRam),
-}
+    /* Limpa o console antes de enviar nova mensagem */
+    console.clear();
+    console.table(stats);
 
-console.log(stats);
+    //exportando informações de stats
+    exports.stats = stats;    
+}, 1000);
+
